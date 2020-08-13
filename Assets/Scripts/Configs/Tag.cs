5cbec6bc874fb95c0ce92927f601531c8f4fd286
ktzwnoy <315kitatas10703@gmail.com>
2020-08-13 22:31:13 +0900

    add: エフェクトの追加

--- Assets/Scripts/Configs/Tag.cs
+++ Assets/Scripts/Configs/Tag.cs
@@ -4,5 +4,6 @@ namespace Configs
     {
         public const string PLAYER = "Player";
         public const string STAGE_OBJECT = "StageObject";
+        public const string GAME_OVER_AREA = "GameOverArea";
     }
 }
--- Assets/Scripts/Games/StageObjects/StageObject.cs
+++ Assets/Scripts/Games/StageObjects/StageObject.cs
@@ -1,16 +1,41 @@
+using Configs;
 using DG.Tweening;
+using Games.Controllers;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
+using Zenject;
 
 namespace Games.StageObjects
 {
     public sealed class StageObject : MonoBehaviour
     {
+        [SerializeField] private GameObject effect = null;
+
         private readonly float _animationTime = 0.5f;
+
+        private GameController _gameController;
+
+        [Inject]
+        private void Construct(GameController gameController)
+        {
+            _gameController = gameController;
+        }
 
         private void Start()
         {
             transform
                 .DOScale(transform.localScale * 2f, _animationTime);
+
+            this.OnTriggerEnter2DAsObservable()
+                .Where(other => other.CompareTag(Tag.GAME_OVER_AREA))
+                .Subscribe(_ =>
+                {
+                    Instantiate(effect, transform.position, Quaternion.identity);
+                    Destroy(gameObject);
+                    _gameController.SetGameOver();
+                })
+                .AddTo(this);
         }
     }
 }
