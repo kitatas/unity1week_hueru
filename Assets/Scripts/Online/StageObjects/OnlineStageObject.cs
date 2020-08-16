aabd40dc22dffc132137ed9bc9371733890a449e
ktzwnoy <315kitatas10703@gmail.com>
2020-08-16 20:18:58 +0900

    add: StageObject生成時にエフェクト追加

--- Assets/Scripts/Online/StageObjects/OnlineStageObject.cs
+++ Assets/Scripts/Online/StageObjects/OnlineStageObject.cs
@@ -11,7 +11,8 @@ namespace Online.StageObjects
 {
     public sealed class OnlineStageObject : MonoBehaviour
     {
-        [SerializeField] private GameObject effect = null;
+        [SerializeField] private GameObject generateEffect = null;
+        [SerializeField] private GameObject explodeEffect = null;
 
         private readonly float _animationTime = 0.5f;
 
@@ -28,6 +29,7 @@ namespace Online.StageObjects
         private void Start()
         {
             _seController.PlaySe(SeType.Generate);
+            Instantiate(generateEffect, transform.position, Quaternion.identity);
 
             transform
                 .DOScale(transform.localScale * 2f, _animationTime);
@@ -37,7 +39,7 @@ namespace Online.StageObjects
                 .Subscribe(_ =>
                 {
                     _seController.PlaySe(SeType.Explode);
-                    Instantiate(effect, transform.position, Quaternion.identity);
+                    Instantiate(explodeEffect, transform.position, Quaternion.identity);
                     Destroy(gameObject);
                     _onlineGameController.SetGameOver();
                 })
