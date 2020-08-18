1ee93d6dbefb59dcedd6959c0092519916bc9372
ktzwnoy <315kitatas10703@gmail.com>
2020-08-18 16:54:52 +0900

    add: タイトル画面の演出追加

--- Assets/Scripts/Games/Controllers/TitleController.cs
+++ Assets/Scripts/Games/Controllers/TitleController.cs
@@ -1,5 +1,10 @@
+using System.Threading;
 using Configs;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using Games.Sounds;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 using Zenject;
 
@@ -7,10 +12,42 @@ namespace Games.Controllers
 {
     public sealed class TitleController : MonoBehaviour
     {
+        [SerializeField] private RectTransform[] titleStrings = null;
+
+        private readonly float _z = 10f;
+
         [Inject]
         private void Construct(BgmController bgmController)
         {
             bgmController.PlayBgm(BgmType.Main);
+
+            var token = this.GetCancellationTokenOnDestroy();
+            foreach (var titleString in titleStrings)
+            {
+                var z = Random.Range(-_z, _z);
+                titleString.localRotation = Quaternion.Euler(new Vector3(0f, 0f, z));
+
+                TweenStringAsync(titleString, token).Forget();
+            }
+        }
+
+        private async UniTaskVoid TweenStringAsync(RectTransform titleString, CancellationToken token)
+        {
+            var animationTime = Random.Range(0.5f, 1f);
+            await titleString
+                .DOLocalRotate(new Vector3(0f, 0f, _z), animationTime)
+                .WithCancellation(token);
+
+            var sequence = DOTween.Sequence()
+                .Append(titleString
+                    .DOLocalRotate(new Vector3(0f, 0f, -_z), animationTime))
+                .Append(titleString
+                    .DOLocalRotate(new Vector3(0f, 0f, _z), animationTime))
+                .SetLoops(-1, LoopType.Yoyo);
+
+            this.OnDisableAsObservable()
+                .Subscribe(_ => sequence?.Kill())
+                .AddTo(this);
         }
     }
 }
